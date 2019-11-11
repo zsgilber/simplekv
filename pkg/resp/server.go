2a9b0a7cc13807c542a0bd3b5367f32633b809bf
Zach Gilbert <zach@myndshft.com>
2019-11-10 21:50:04 -0700

    use kv store interface

--- pkg/resp/server.go
+++ pkg/resp/server.go
@@ -4,18 +4,22 @@ import (
 	"fmt"
 	"net"
 	"sync"
+
+	"github.com/zsgilber/simplekv/pkg/kv"
 )
 
 // Server defines a server for listening to client requests.
 type Server struct {
 	mu       sync.RWMutex
 	handlers map[string]func(resp *RespConn)
+	store    kv.Store
 }
 
 // NewServer creates a new server with an empty handlers map.
-func NewServer() *Server {
+func NewServer(store kv.Store) *Server {
 	return &Server{
 		handlers: make(map[string]func(resp *RespConn)),
+		store:    store,
 	}
 }
 
@@ -25,7 +29,7 @@ func (s *Server) AddHandleFunc(command string, handler func(resp *RespConn)) {
 	s.handlers[command] = handler
 }
 
-func (s *Server) ListenAndServe(address string, kv map[string]string) error {
+func (s *Server) ListenAndServe(address string) error {
 	listener, err := net.Listen("tcp", address)
 	if err != nil {
 		return err
@@ -37,11 +41,11 @@ func (s *Server) ListenAndServe(address string, kv map[string]string) error {
 			fmt.Println(err)
 			continue
 		}
-		go s.handleConnection(conn, kv)
+		go s.handleConnection(conn, s.store)
 	}
 }
 
-func (s *Server) handleConnection(conn net.Conn, kv map[string]string) error {
+func (s *Server) handleConnection(conn net.Conn, store kv.Store) error {
 	respConn := NewRespConn(conn)
 	command, err := respConn.ReadCommand()
 	if err != nil {
@@ -52,13 +56,15 @@ func (s *Server) handleConnection(conn net.Conn, kv map[string]string) error {
 	fmt.Println(len(commandName))
 	switch commandName {
 	case "set":
-		fmt.Println("here")
 		fmt.Printf("set key %v to value %v", string(command.Args[1].str), string(command.Args[2].str))
-		kv[string(command.Args[1].str)] = string(command.Args[2].str)
+		store.Set(string(command.Args[1].str), string(command.Args[2].str))
 		return nil
 	case "get":
-		fmt.Println("inside get")
-		fmt.Println(kv[string(command.Args[1].str)])
+		value, err := store.Get(string(command.Args[1].str))
+		fmt.Println(value)
+		if err != nil {
+			return err
+		}
 	default:
 		fmt.Println("unknown command")
 	}
